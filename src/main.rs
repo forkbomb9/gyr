518d4311052a3166ede4818c664aa9b9e927f990
Namkhai B <namkhai.n3@protonmail.com>
2020-02-20 12:23:48 -0600

    refactor: Big code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 mod event;
 
+use std::collections::HashMap;
 use std::convert;
 use std::fmt;
 use std::fs;
@@ -34,7 +35,6 @@ fn read_applications(dirs: Vec<impl Into<path::PathBuf>>) -> Result<Vec<Applicat
 
         for file in files {
             if let Ok(file) = file {
-                // dbg!(&file);
                 let contents = fs::read_to_string(file.path())?;
                 if contents.starts_with("[Desktop Entry]") {
                     let contents = contents.trim_start_matches("[Desktop Entry]\n");
@@ -50,7 +50,6 @@ fn read_applications(dirs: Vec<impl Into<path::PathBuf>>) -> Result<Vec<Applicat
 
     Ok(apps)
 }
-// }
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 struct Application {
@@ -66,6 +65,7 @@ impl fmt::Display for Application {
     }
 }
 
+// This is needed for the SelectableList widget.
 impl convert::AsRef<str> for Application {
     fn as_ref(&self) -> &str {
         self.name.as_ref()
@@ -75,25 +75,23 @@ impl convert::AsRef<str> for Application {
 impl Application {
     pub fn parse<T: Into<String>>(contents: T) -> Result<Application, failure::Error> {
         let contents = contents.into();
-        let mut queries = std::collections::HashMap::new();
+        let mut values = HashMap::new();
         let keys: Vec<&str> = contents.split('\n').collect();
 
-        // dbg!(&keys);
         if keys.len() > 0 {
             for key in keys {
                 if key != "" {
                     let splitted = key.split('=').collect::<Vec<&str>>();
                     if splitted.len() == 2 {
-                        queries.entry(splitted[0]).or_insert(splitted[1]);
+                        values.entry(splitted[0]).or_insert(splitted[1]);
                     } else {
-                        queries.entry(splitted[0]).or_insert("empty");
+                        values.entry(splitted[0]).or_insert("empty");
                     }
                 }
             }
         }
-        // dbg!(&queries);
 
-        if let Some(key) = queries.get("NoDisplay") {
+        if let Some(key) = values.get("NoDisplay") {
             if key.to_lowercase() == "true" {
                 failure::bail!("App is hidden");
             }
@@ -102,7 +100,7 @@ impl Application {
         let exec_trimmed;
         {
             let re = Regex::new(r" ?%[cDdFfikmNnUuv]").unwrap();
-            let mut exec = queries.get("Exec").unwrap_or(&"Unknow").to_string();
+            let mut exec = values.get("Exec").unwrap_or(&"Unknow").to_string();
 
             if let Some(range) = re.find(&exec.clone()) {
                 exec.replace_range(range.start()..range.end(), "");
@@ -110,23 +108,23 @@ impl Application {
             exec_trimmed = exec;
         }
 
-        let terminal_exec = if queries.get("Terminal").unwrap_or(&"false") == &"true" {
+        let terminal_exec = if values.get("Terminal").unwrap_or(&"false") == &"true" {
             true
         } else {
             false
         };
 
         Ok(Application {
-            name: queries.get("Name").unwrap_or(&"Unknow").to_string(),
+            name: values.get("Name").unwrap_or(&"Unknow").to_string(),
             exec: exec_trimmed,
-            description: queries.get("Comment").unwrap_or(&"Unknow").to_string(),
+            description: values.get("Comment").unwrap_or(&"Unknow").to_string(),
             terminal_exec: terminal_exec,
         })
     }
 }
 
 struct App<'a> {
-    items: Vec<Application>,
+    hidden: Vec<Application>,
     shown: Vec<Application>,
     selected: Option<usize>,
     text: Vec<Text<'a>>,
@@ -136,20 +134,17 @@ struct App<'a> {
 
 impl<'a> App<'a> {
     fn new(items: Vec<Application>) -> App<'a> {
-        let mut app = App {
-            shown: Vec::new(),
-            items: items,
+        App {
+            shown: items,
+            hidden: vec![],
             selected: Some(0),
-            text: Vec::new(),
+            text: vec![],
             query: String::new(),
-            log: Vec::new(),
-        };
-        for item in &app.items {
-            app.shown.push(item.clone());
+            log: vec![],
         }
-        app
     }
-    fn update(&mut self) {
+
+    fn update_info(&mut self) {
         if let Some(selected) = self.selected {
             self.text = vec![
                 Text::styled(
@@ -166,21 +161,37 @@ impl<'a> App<'a> {
             self.text.clear();
         }
     }
+
     fn update_filter(&mut self) {
-        // let mut i = 0;
-        // while i != self.shown.len() {
-        //     if !self.shown[i].name.to_lowercase().starts_with(&self.query.to_lowercase()) {
-        //         &self.shown.remove(i);
-        //     } else {
-        //         i += 1;
-        //     }
-        // }
-        self.shown.clear();
-        for item in &self.items {
-            if matches(&self.query, &item.name.to_lowercase()) {
+        // I could use self.hidden.push(
+        //                  self.shown.drain_filter(
+        //                      |i| !matches(&self.query, n.lo_lowercase()
+        //                  )
+        //              ));
+        // but Vec::drain_filter() it's nightly-only (for now)
+        let mut i = 0;
+        while i != self.shown.len() {
+            if !matches(&self.query, &self.shown[i].name.to_lowercase()) {
+                &self.hidden.push(self.shown.remove(i));
+            } else {
+                i += 1;
+            }
+        }
+
+        for item in &self.hidden {
+            if matches(&self.query, &item.name.to_lowercase()) && !self.shown.contains(item) {
                 self.shown.push(item.clone());
             }
         }
+
+        self.shown.sort();
+
+        // self.shown.clear();
+        // for item in &self.items {
+        //     if matches(&self.query, &item.name.to_lowercase()) {
+        //         self.shown.push(item.clone());
+        //     }
+        // }
 
         if self.shown.is_empty() {
             self.selected = None;
@@ -196,19 +207,11 @@ impl<'a> App<'a> {
 }
 
 fn main() -> Result<(), failure::Error> {
-    // let contents = fs::read_to_string("/usr/share/applications/firefox.desktop")?;
-    // let contents = contents.trim_start_matches("[Desktop Entry]\n");
-    // dbg!(Application::parse(contents));
-
-    // return Ok(());
-
-    // let dirs = vec!["/usr/share/applications", "/usr/local/share/applications", "~/.local/share/applications"];
     let mut dirs: Vec<path::PathBuf> = vec![];
-    let local_share_applications = env!("HOME").to_string() + "/.local/share/applications";
     for dir in &[
-        "/usr/share/applications",
-        "/usr/local/share/applications",
-        &local_share_applications,
+        "/usr/share/applications".to_string(),
+        "/usr/local/share/applications".to_string(),
+        env!("HOME").to_string() + "/.local/share/applications",
     ] {
         let path = path::PathBuf::from(dir);
         if path.exists() {
@@ -217,7 +220,6 @@ fn main() -> Result<(), failure::Error> {
     }
 
     let apps = read_applications(dirs)?;
-    // println!("Number of apps: {}", apps.len());
 
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode()?;
@@ -232,10 +234,8 @@ fn main() -> Result<(), failure::Error> {
     // App
     let mut app = App::new(apps);
 
-    app.update();
+    app.update_info();
 
-    // let mut scroll: u16 = 0;
-    // let mut top_block_height: Option<usize> = None;
     loop {
         terminal.draw(|mut f| {
             let chunks = Layout::default()
@@ -249,11 +249,20 @@ fn main() -> Result<(), failure::Error> {
                 .borders(Borders::ALL)
                 .title_style(Style::default().modifier(Modifier::BOLD));
 
-            let bottom_block = Layout::default()
+            let bottom_chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints([Constraint::Min(3), Constraint::Length(3)].as_ref())
                 .split(chunks[1]);
 
+            // Text for description
+            Paragraph::new(app.text.iter())
+                .block(block.title("WLauncher"))
+                .style(Style::default())
+                .alignment(Alignment::Left)
+                .wrap(true)
+                .render(&mut f, chunks[0]);
+
+            // App list
             SelectableList::default()
                 .block(block.title("Apps").borders(Borders::ALL))
                 .items(&app.shown)
@@ -261,8 +270,9 @@ fn main() -> Result<(), failure::Error> {
                 .style(style)
                 .highlight_style(style.fg(Color::LightBlue).modifier(Modifier::BOLD))
                 .highlight_symbol(">")
-                .render(&mut f, bottom_block[0]);
+                .render(&mut f, bottom_chunks[0]);
 
+            // Query
             Paragraph::new(
                 [
                     Text::styled(">", Style::default().fg(Color::LightBlue)),
@@ -276,31 +286,7 @@ fn main() -> Result<(), failure::Error> {
             .style(Style::default())
             .alignment(Alignment::Left)
             .wrap(true)
-            .render(&mut f, bottom_block[1]);
-
-            // let top_block = Layout::default()
-            //     .direction(Direction::Horizontal)
-            //     .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-            //     .split(chunks[0]);
-
-            // Text for description
-            Paragraph::new(app.text.iter())
-                .block(block.title("WLauncher"))
-                .style(Style::default())
-                .alignment(Alignment::Left)
-                .wrap(true)
-                .render(&mut f, chunks[0]);
-            // .render(&mut f, top_block[0]);
-
-            // Paragraph::new(app.log.iter())
-            //     .block(block.title("Log"))
-            //     .style(Style::default())
-            //     .alignment(Alignment::Left)
-            //     .wrap(true)
-            //     // .scroll(scroll)
-            //     .render(&mut f, top_block[1]);
-
-            // top_block_height = top_block[1].height.try_into().ok();
+            .render(&mut f, bottom_chunks[1]);
         })?;
 
         match events.next()? {
@@ -309,17 +295,6 @@ fn main() -> Result<(), failure::Error> {
                     return Ok(());
                 }
                 Key::Char('\n') => {
-                    // app.log.push(Text::raw(format!(
-                    //     "I should run '{}'\n",
-                    //     app.shown[app.selected].exec
-                    // )));
-
-                    // if let Some(height) = top_block_height {
-                    //     if app.log.len() > height - 2 {
-                    //         // app.log.insert(0, Text::styled("Overflow\n", Style::default().bg(Color::LightRed)));
-                    //         scroll += 1;
-                    //     }
-                    // }
                     break;
                 }
                 Key::Char(c) => {
@@ -332,11 +307,9 @@ fn main() -> Result<(), failure::Error> {
                 }
                 Key::Left => {
                     app.selected = Some(0);
-                    // app.update();
                 }
                 Key::Right => {
                     app.selected = Some(app.shown.len() - 1);
-                    // app.update();
                 }
                 Key::Down => {
                     if let Some(selected) = app.selected {
@@ -346,8 +319,6 @@ fn main() -> Result<(), failure::Error> {
                             Some(selected + 1)
                         };
                     }
-
-                    // app.update();
                 }
                 Key::Up => {
                     if let Some(selected) = app.selected {
@@ -357,15 +328,13 @@ fn main() -> Result<(), failure::Error> {
                             Some(app.shown.len() - 1)
                         };
                     }
-
-                    // app.update();
                 }
                 _ => {}
             },
             Event::Tick => (),
         }
 
-        app.update();
+        app.update_info();
     }
 
     if let Some(selected) = app.selected {
