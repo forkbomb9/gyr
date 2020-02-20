274df9011f6e78eeafd17bb23b57f8d782435825
Namkhai B <namkhai.n3@protonmail.com>
2020-02-20 17:30:09 -0600

    feat: Small interface changes

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,7 @@ impl Application {
         let exec_trimmed;
         {
             let re = Regex::new(r" ?%[cDdFfikmNnUuv]").unwrap();
-            let mut exec = values.get("Exec").unwrap_or(&"Unknow").to_string();
+            let mut exec = values.get("Exec").unwrap_or(&"unknow").to_string();
 
             if let Some(range) = re.find(&exec.clone()) {
                 exec.replace_range(range.start()..range.end(), "");
@@ -117,7 +117,7 @@ impl Application {
         Ok(Application {
             name: values.get("Name").unwrap_or(&"Unknow").to_string(),
             exec: exec_trimmed,
-            description: values.get("Comment").unwrap_or(&"Unknow").to_string(),
+            description: values.get("Comment").unwrap_or(&"No description").to_string(),
             terminal_exec: terminal_exec,
         })
     }
@@ -152,8 +152,9 @@ impl<'a> App<'a> {
                     Style::default().fg(Color::LightBlue),
                 ),
                 Text::raw(format!("{}\n", &self.shown[selected].description)),
+                Text::raw("\nExec: "),
                 Text::styled(
-                    format!("\n{}", &self.shown[selected].exec),
+                    format!("{}", &self.shown[selected].exec),
                     Style::default().fg(Color::DarkGray),
                 ),
             ];
